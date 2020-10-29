f1c677dce5513c30bf78c8f3e826f354935a286e
Brian Demro <demrobrian@hotmail.com>
2020-10-28 19:58:44 -0500

    updated

--- week4-day3-afternoon/src/Components/Header.js
+++ week4-day3-afternoon/src/Components/Header.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
+import logo from "../navbar-logo.svg"
 
-class Header extends Component {
-    render() {
+function Header(){
         return (
-            <div>
-               <header>
-                    <div>
-                        <a>
-                            <img/>
-                        </a>
-                    </div>
-                    <ul className="nav-link-box">
-                        <li className="nav-link">Services</li>
-                        <li className="nav-link">Portfolio</li>
-                        <li className="nav-link">About</li>
-                        <li className="nav-link">Team</li>
-                        <li className="nav-link">contact</li>
-                    </ul>
-                 </header> 
-         </div>
+            <header className='welcome-header'>
+                <div className="logo-menu-box">
+                    <a href="https://localhost:3000">
+                        <img className='logo-svg' src={logo} alt=""/>
+                    </a>
+                    <button className="menu-button-mobile">Menu &#9776;</button>
+                </div>
+                <ul className="nav-link-box">
+                    <li className="nav-link">SERVICE</li>
+                    <li className="nav-link">PORTFOLIO</li>
+                    <li className="nav-link">ABOUT</li>
+                    <li className="nav-link">TEAM</li>
+                    <li className="nav-link">CONTACT</li>
+                </ul>
+            </header> 
         )
-    }
 }
 
 export default Header
--- week4-day3-afternoon/src/Components/Welcome.js
+++ week4-day3-afternoon/src/Components/Welcome.js
@@ -5,11 +5,11 @@ import Header from './Header'
 function Welcome(){
         return (
             <div className="welcome-box">
-                <Header/>
+                <Header />
                 <div className="welcome-content">
-                    <h2></h2>
-                    <h1></h1>
-                    <button className='tell-me-more'>Tell Mee More</button>
+                    <h2>Welcome to our Studio!</h2>
+                    <h1>IT'S NICE TO MEET YOU</h1>
+                    <button className='tell-me-more'>TELL ME MORE</button>
                 </div>   
             </div>
         )
